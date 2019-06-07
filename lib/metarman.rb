ec7a5ab40d1cadb132399d7b5eb11f326b495f09
Keisuke Mori <rjfs29kei@gmail.com>
2019-06-07 15:23:49 +0900

    add module method

--- lib/metarman.rb
+++ lib/metarman.rb
@@ -7,4 +7,12 @@ require "metarman/metar_parser"
 module Metarman
   class Error < StandardError;
   end
+
+  def self.get_raw(icao)
+    Weather.new(icao).get_raw
+  end
+
+  def self.get_with_info(icao)
+    Weather.new(icao).get_with_info
+  end
 end
