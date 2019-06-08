2e14469a1c442ba44c72d0b4544755d29048a2b7
Keisuke Mori <rjfs29kei@gmail.com>
2019-06-09 06:17:36 +0900

    fix cannnot access csv-path issue

--- lib/metarman/data.rb
+++ lib/metarman/data.rb
@@ -11,7 +11,8 @@ module Metarman
     # return airport inforomation :Hash like this {"id"=>"2313", "name"=>"Nagasaki Airport", "icao"=>"RJFU", "iata"=>"NGS", "country"=>"Japan", "city_name"=>"Nagasaki", "lat"=>"32.916900634799994", "lon"=>"129.914001465", "elev"=>"15", "utc"=>"9", "dst"=>"U", "timezone"=>"Asia/Tokyo"}
     def get
       # This airport information data is from https://openflights.org/ and edited by author.
-      CSV.foreach("./lib/metarman/airports.csv", headers: true) do |row|
+      csv_path = File.join( File.dirname(__FILE__), 'airports.csv' )
+      CSV.foreach(csv_path, headers: true) do |row|
         @data[row["icao"]] << row
       end
       @airport = @data[@icao].first.to_h
