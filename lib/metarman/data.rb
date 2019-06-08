a6f9717a76c3f8924332716ccaad62e23bab0dcc
Keisuke Mori <rjfs29kei@gmail.com>
2019-06-09 06:18:22 +0900

    fix typo

--- lib/metarman/data.rb
+++ lib/metarman/data.rb
@@ -8,7 +8,7 @@ module Metarman
       @airport = nil
     end
 
-    # return airport inforomation :Hash like this {"id"=>"2313", "name"=>"Nagasaki Airport", "icao"=>"RJFU", "iata"=>"NGS", "country"=>"Japan", "city_name"=>"Nagasaki", "lat"=>"32.916900634799994", "lon"=>"129.914001465", "elev"=>"15", "utc"=>"9", "dst"=>"U", "timezone"=>"Asia/Tokyo"}
+    # return airport information :Hash like this {"id"=>"2313", "name"=>"Nagasaki Airport", "icao"=>"RJFU", "iata"=>"NGS", "country"=>"Japan", "city_name"=>"Nagasaki", "lat"=>"32.916900634799994", "lon"=>"129.914001465", "elev"=>"15", "utc"=>"9", "dst"=>"U", "timezone"=>"Asia/Tokyo"}
     def get
       # This airport information data is from https://openflights.org/ and edited by author.
       csv_path = File.join( File.dirname(__FILE__), 'airports.csv' )
